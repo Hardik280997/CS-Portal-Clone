8193ebaa10c86d3fef83ca1ddbf08f1967aa23e0
Hardik Patel <hardik.p@crestinfosystems.com>
2025-02-07 16:26:17 +0530

    Resolve no file found issue

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,8 +1,7 @@
 export default async function Layout({ children }: { children: React.ReactNode }) {
     return (
         <div>
-            {children}
-            
+            {children} 
         </div>
     )
 }
